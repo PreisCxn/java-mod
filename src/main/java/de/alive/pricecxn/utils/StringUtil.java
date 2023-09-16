7320307e8621cc15c2d654a9a9a55d78956b9d88
Alive <nicolasfr002@gmail.com>
2023-09-16 20:19:10 +0200

    deleted unnecessary functions

--- src/main/java/de/alive/pricecxn/utils/StringUtil.java
+++ src/main/java/de/alive/pricecxn/utils/StringUtil.java
@@ -21,46 +21,6 @@ import java.util.Objects;
  */
 public class StringUtil {
 
-    public static Pair<String, Formatting> TextComponent(String str, Formatting formatting) {
-        return Pair.of(str, formatting);
-    }
-
-    public static MutableText getColorizedString(String string, Formatting formatting){
-        return MutableText.of(new LiteralTextContent(string)).setStyle(Style.EMPTY.withColor(formatting));
-    }
-
-    public static MutableText getColorizedString(List<Pair<String, Formatting>> parts) {
-
-        String first = parts.get(0).getFirst();
-
-        MutableText msg = StringUtil.getColorizedString(parts.get(0).getFirst(), parts.get(0).getSecond());
-
-        for (Pair<String, Formatting> part : parts) {
-
-            if(Objects.equals(part.getFirst(), first)) continue;
-
-            msg.append(StringUtil.getColorizedString(part.getFirst(), part.getSecond()));
-        }
-
-        return msg;
-    }
-
-    /**
-     * This method converts all Strings in a list to lower case
-     * @param list The list to convert
-     * @return The converted list
-     */
-    public static String[] listToLowerCase(@Nullable String[] list) {
-        if(list == null) return null;
-
-        String[] newList = new String[list.length];
-        for (int i = 0; i < list.length; i++) {
-            if(list[i] != null)
-                newList[i] = list[i].toLowerCase();
-        }
-        return newList;
-    }
-
     /**
      * This method converts all Strings in a list to lower case
      * @param list The list to convert
