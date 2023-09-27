23267bdd8d757cc70c035ea3e0187292473c13ad
Alive <nicolasfr002@gmail.com>
2023-09-27 15:59:05 +0200

    added @Shadow variables to ItemStackMixin

--- src/main/java/de/alive/pricecxn/mixin/ItemStackMixin.java
+++ src/main/java/de/alive/pricecxn/mixin/ItemStackMixin.java
@@ -2,9 +2,11 @@ package de.alive.pricecxn.mixin;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,6 +16,18 @@ import java.util.List;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+
+    @Shadow
+    public abstract boolean isEmpty();
+
+    @Shadow
+    public abstract void removeCustomName();
+
+    @Shadow
+    public abstract Item getItem();
+
+    @Shadow
+    private int count;
 
     @Inject(method = "getTooltip", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     private void getToolTip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> callbackInfoReturnable, List<Text> list) {
