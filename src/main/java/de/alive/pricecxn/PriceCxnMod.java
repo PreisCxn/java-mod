1e8f60c025eccfed4425209fdac4c8b44216217c
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com>
2024-03-24 15:37:08 +0100

    [Bugfix] Removed hardcoded version

--- src/main/java/de/alive/pricecxn/PriceCxnMod.java
+++ src/main/java/de/alive/pricecxn/PriceCxnMod.java
@@ -34,7 +34,7 @@ public class PriceCxnMod implements ModInitializer {
 					.setStyle(Style.EMPTY.withColor(Formatting.GOLD)))
 			.append(MutableText.of(new PlainTextContent.Literal("] "))
 					.setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY)));
-	public static final String MOD_VERSION = "2.0.1";//Version.MOD_VERSION;
+	public static final String MOD_VERSION = Version.MOD_VERSION;
 
 	@Override
 	public void onInitialize() {
