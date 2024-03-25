a16ababf02fc541e3191327d926605e530f9c2d1
Thorge Engelmann <56771258+TeddyBear2004@users.noreply.github.com>
2024-03-25 16:41:16 +0100

    [Patch] Patched dependencies to newer versions

--- src/main/java/de/alive/pricecxn/networking/sockets/SocketOpenListener.java
+++ src/main/java/de/alive/pricecxn/networking/sockets/SocketOpenListener.java
@@ -1,6 +1,6 @@
 package de.alive.pricecxn.networking.sockets;
 
-import javax.websocket.Session;
+import jakarta.websocket.Session;
 
 public interface SocketOpenListener {
     void onOpen(Session session);
