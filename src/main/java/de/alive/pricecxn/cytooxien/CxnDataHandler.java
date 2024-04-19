9397cf1ef73ecc8f055ecf147220fa888c85bb99
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com>
2024-04-19 21:44:12 +0200

    [PCXN-32]:moved method to CxnDataHandler
    Took 2 minutes

--- src/main/java/de/alive/pricecxn/cytooxien/CxnDataHandler.java
+++ src/main/java/de/alive/pricecxn/cytooxien/CxnDataHandler.java
@@ -1,6 +1,7 @@
 package de.alive.pricecxn.cytooxien;
 
 import com.google.gson.JsonObject;
+import de.alive.pricecxn.networking.DataAccess;
 import de.alive.pricecxn.networking.DataHandler;
 import de.alive.pricecxn.networking.ServerChecker;
 import de.alive.pricecxn.networking.sockets.WebSocketCompletion;
@@ -82,7 +83,36 @@ public class CxnDataHandler {
     }
 
     private void createTranslationHandler(@NotNull List<String> langList) {
-        // Method implementation...
+        DataAccess[] translationAccess = {
+                TranslationDataAccess.INV_AUCTION_HOUSE_SEARCH,
+                TranslationDataAccess.INV_ITEM_SHOP_SEARCH,
+                TranslationDataAccess.INV_NOOK_SEARCH,
+                TranslationDataAccess.INV_TRADE_SEARCH,
+                TranslationDataAccess.TIMESTAMP_SEARCH,
+                TranslationDataAccess.SELLER_SEARCH,
+                TranslationDataAccess.BID_SEARCH,
+                TranslationDataAccess.AH_BUY_SEARCH,
+                TranslationDataAccess.THEME_SERVER_SEARCH,
+                TranslationDataAccess.HIGHEST_BIDDER_SEARCH,
+                TranslationDataAccess.NOOK_BUY_SEARCH,
+                TranslationDataAccess.SHOP_BUY_SEARCH,
+                TranslationDataAccess.SHOP_SELL_SEARCH,
+                TranslationDataAccess.TRADE_BUY_SEARCH,
+                TranslationDataAccess.HOUR_SEARCH,
+                TranslationDataAccess.MINUTE_SEARCH,
+                TranslationDataAccess.SECOND_SEARCH,
+                TranslationDataAccess.NOW_SEARCH,
+                TranslationDataAccess.SKYBLOCK_INV_BLOCK,
+                TranslationDataAccess.CITYBUILD_INV_BLOCK
+        };
+
+
+        putData("cxnprice.translation",
+                            new DataHandler(serverChecker,
+                                            "/settings/translations",
+                                            langList,
+                                            "translation_key",
+                                            DataHandler.TRANSLATION_REFRESH_INTERVAL, translationAccess));
     }
 
     public DataHandler getData(String key) {
