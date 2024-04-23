6b28ebd1906298c6a99a2979f37c4322da3740b8
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com>
2024-04-23 19:58:59 +0200

    Bug fixes regarding listener_sender.py workflow
    Took 2 minutes

--- CdnSender/listener_sender.py
+++ CdnSender/listener_sender.py
@@ -2,6 +2,7 @@ from file_sender import upload_file, get_version_from_build_gradle
 
 file_path = "../Listener/build/libs/Listener.jar"
 
-url = f"https://cdn.preiscxn.de/Listener.jar?version={get_version_from_build_gradle("Listener")}"
+version = get_version_from_build_gradle("Listener")
+url = f"https://cdn.preiscxn.de/Listener.jar?version={version}"
 
 upload_file(file_path, url)
