37f4a92818a7cb6a93d2cc7095c67c2db86c4913
Thorge Engelmann <56771258+TeddyBear2004@users.noreply.github.com.>
2024-05-21 09:58:42 +0200

    (PCXN-84):Bug fixes regarding file system

--- impl/src/main/java/de/alive/preiscxn/impl/modules/ClasspathModule.java
+++ impl/src/main/java/de/alive/preiscxn/impl/modules/ClasspathModule.java
@@ -3,9 +3,16 @@ package de.alive.preiscxn.impl.modules;
 import de.alive.preiscxn.api.PriceCxn;
 import de.alive.preiscxn.api.module.Module;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
-import java.nio.file.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,20 +37,30 @@ public class ClasspathModule implements Module {
                 URL url = resources.nextElement();
                 URI uri = url.toURI();
                 Map<String, String> env = new HashMap<>();
-
                 Path path;
+
                 if ("jar".equals(uri.getScheme())) {
                     String[] array = uri.toString().split("!");
-                    try (FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env)) {
-                        path = fs.getPath(array[1]);
-                        processPath(path, consumer);
+                    try {
+                        try (FileSystem fs = getFileSystem(URI.create(array[0]), env)) {
+                            path = fs.getPath(array[1]);
+                            processPath(path, consumer);
+                        }
                     } catch (FileSystemNotFoundException | FileSystemAlreadyExistsException e) {
                         try {
-                            path = Paths.get(URI.create(array[0])); // Handle jar within a nested jar
+                            String jarUri = array[0].replace("jar:file:", "");
+                            if (!jarUri.contains("!/")) {
+                                jarUri += "!/";
+                            }
+                            try (FileSystem fs = getFileSystem(URI.create("jar:file:" + jarUri), env)) {
+                                path = fs.getPath(array[1]);
+                            }
                             processPath(path, consumer);
-                        }catch (Exception ex){
-                            //this is not supposed to be, but if it works, its fine.
+                        } catch (Exception ex) {
+                            PriceCxn.getMod().getLogger().error("Error while loading module", ex);
                         }
+                    }catch (Exception ex){
+                        PriceCxn.getMod().getLogger().error("Error while loading module", ex);
                     }
                 } else {
                     path = Paths.get(uri);
@@ -54,6 +71,15 @@ public class ClasspathModule implements Module {
             PriceCxn.getMod().getLogger().error("Error while loading module", e);
         }
     }
+
+    private FileSystem getFileSystem(URI uri, Map<String, String> env) throws IOException {
+        try {
+            return FileSystems.newFileSystem(uri, env);
+        } catch (FileSystemAlreadyExistsException e) {
+            return FileSystems.getFileSystem(uri);
+        }
+    }
+
 
     private void processPath(Path path, Consumer<Class<?>> consumer) throws Exception {
         try (Stream<Path> pathStream = Files.walk(path)) {
