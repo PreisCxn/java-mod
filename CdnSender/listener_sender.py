5e19ffc458113cda00700237f4b53331af8f26f8
Thorge Engelmann <56771258+TeddyBear2004@users.noreply.github.com.>
2024-05-22 16:54:22 +0200

    fixed workflow

--- CdnSender/listener_sender.py
+++ CdnSender/listener_sender.py
@@ -2,10 +2,10 @@ import os
 
 from file_sender import upload_file, select_file, get_version
 
-selected_file = select_file('./Listener/build/libs', 'listener')
+selected_file = select_file('./listener/build/libs', 'listener')
 
 if selected_file:
-    file_path = os.path.join('./Listener/build/libs', selected_file)
+    file_path = os.path.join('./listener/build/libs', selected_file)
 else:
     exit(-1)
 
