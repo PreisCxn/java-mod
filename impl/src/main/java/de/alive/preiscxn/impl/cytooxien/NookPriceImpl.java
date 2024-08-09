c9f61979fa012fb2a38d60abf7d52adcf5f3e688
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com.>
2024-08-09 23:26:24 +0200

    bug fixes
    Took 1 hour 55 minutes

--- impl/src/main/java/de/alive/preiscxn/impl/cytooxien/NookPriceImpl.java
+++ impl/src/main/java/de/alive/preiscxn/impl/cytooxien/NookPriceImpl.java
@@ -3,23 +3,21 @@ package de.alive.preiscxn.impl.cytooxien;
 import com.google.gson.JsonObject;
 import de.alive.preiscxn.api.cytooxien.NookPrice;
 
-import java.util.Objects;
-
 public class NookPriceImpl implements NookPrice {
     private final JsonObject itemInfo;
 
     public NookPriceImpl(JsonObject itemInfo) {
-        this.itemInfo = Objects.requireNonNullElse(itemInfo == null || !itemInfo.isJsonObject() ? null : itemInfo, new JsonObject());
+        this.itemInfo = itemInfo;
     }
 
     @Override
     public boolean isEmpty() {
-        return itemInfo == null || itemInfo.isJsonNull();
+        return itemInfo == null;
     }
 
     @Override
     public double getPrice() {
-        if (itemInfo == null || itemInfo.isJsonNull()) return 0;
+        if (itemInfo == null) return 0;
         if (!itemInfo.has("price") || itemInfo.get("price").isJsonNull()) return 0;
         return itemInfo.get("price").getAsDouble();
     }
