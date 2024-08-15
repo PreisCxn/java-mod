a7fb4c5615de9afbe4e97d94dbf332536dec98db
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com.>
2024-08-15 07:31:24 +0200

    [PCXN-90]:bug fixes to ensure compatability with other mods
    Took 2 hours 40 minutes

--- fabric/fabric_core/src/main/java/de/alive/preiscxn/fabric/MixinConfig.java
+++ fabric/fabric_core/src/main/java/de/alive/preiscxn/fabric/MixinConfig.java
@@ -1,42 +1,28 @@
 package de.alive.preiscxn.fabric;
 
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.SharedConstants;
+import com.google.gson.JsonParseException;
+import net.minecraft.MinecraftVersion;
+import net.minecraft.util.JsonHelper;
 import org.objectweb.asm.tree.ClassNode;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Set;
 
 public class MixinConfig implements IMixinConfigPlugin {
 
-    private static final String VERSION;
+    private static String VERSION;
     private static final Logger LOGGER = LoggerFactory.getLogger(MixinConfig.class);
-
-    static {
-
-        String tempVersion;
-        try{
-            SharedConstants.createGameVersion();
-            tempVersion = SharedConstants.getGameVersion().getName().replace(".", "_");
-            LOGGER.info("Loaded Minecraft version for pricecxn mixins: {}", tempVersion);
-        }catch(Exception e){
-            throw new RuntimeException("Failed to get Minecraft version", e);
-        }
-        VERSION = tempVersion;
-    }
-
-    public static boolean isModLoaded(String modid) {
-        return FabricLoader.getInstance().isModLoaded(modid);
-    }
 
     @Override
     public void onLoad(String mixinPackage) {
-
+        VERSION = create().replace(".", "_");;
     }
 
     @Override
@@ -46,8 +32,8 @@ public class MixinConfig implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        boolean contains = mixinClassName.contains(VERSION);
-        LOGGER.info("Mixin {} for {} is {}", mixinClassName, targetClassName, contains ? "enabled" : "disabled");
+        boolean contains = mixinClassName.contains(".v" + VERSION + ".");
+        LOGGER.info("Mixin {} for {} is {} with {}", mixinClassName, targetClassName, contains ? "enabled" : "disabled", VERSION);
         return contains;
     }
 
@@ -71,4 +57,55 @@ public class MixinConfig implements IMixinConfigPlugin {
 
     }
 
+    public static String create() {
+        try {
+            InputStream inputStream = MinecraftVersion.class.getResourceAsStream("/version.json");
+
+            String var10;
+            label63: {
+                String var2;
+                try {
+                    if (inputStream == null) {
+                        LOGGER.warn("Missing version information!");
+                        var10 = "1_21_1";
+                        break label63;
+                    }
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                    try {
+                        var2 = JsonHelper.deserialize(inputStreamReader).get("name").getAsString();
+                    } catch (Throwable var6) {
+                        try {
+                            inputStreamReader.close();
+                        } catch (Throwable var5) {
+                            var6.addSuppressed(var5);
+                        }
+
+                        throw var6;
+                    }
+
+                    inputStreamReader.close();
+                } catch (Throwable var7) {
+                    if (inputStream != null) {
+                        try {
+                            inputStream.close();
+                        } catch (Throwable var4) {
+                            var7.addSuppressed(var4);
+                        }
+                    }
+
+                    throw var7;
+                }
+
+                inputStream.close();
+
+                return var2;
+            }
+
+            return var10;
+        } catch (JsonParseException | IOException var8) {
+            throw new IllegalStateException("Game version information is corrupt", var8);
+        }
+    }
 }
